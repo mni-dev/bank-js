455409ae047aa013e81e51536c96a5c4208fbedc
mni-dev <nazrulislam01531@gmail.com>
2022-02-10 16:18:21 +0600

    deposit update

--- js/banking.js
+++ js/banking.js
@@ -1,18 +1,25 @@
 //handle deposit button
 document.getElementById('deposit-btn').addEventListener('click', function(){
      const depositInput = document.getElementById('deposit-input');
-    //  console.log(deopositInput);
+
+     //Update Deposit Total
     const depositedAmount = depositInput.value;
     // console.log(depositedAmount);
     const depositShowHere = document.getElementById('deposit-total');
-    depositShowHere.innerText = depositedAmount;
-    // console.log(depositShowHere);
     //clear the input field
     depositInput.value = '';
     const prevDepositedAmount = depositShowHere.innerText;
     // console.log(prevDepositedAmount)
-    const newDeposit = depositShowHere.innerText;
-    const currentDeposit = prevDepositedAmount + newDeposit;
-    console.log(currentDeposit)
+    const newDeposit = parseFloat(prevDepositedAmount) + parseFloat(depositedAmount);
+    console.log(newDeposit);
+    const currentDeposit = parseFloat(newDeposit);
+    depositShowHere.innerText = currentDeposit;
+    // console.log(depositShowHere)
+
+    //Update Account Balance
+    const balance = document.getElementById('balance');
+    const prevBalance = balance.innerText;
+    const newBalance = parseFloat(prevBalance) + parseFloat(depositedAmount); //parseFloat(newDeposit);
+    balance.innerText = newBalance;
     
 })
